6c27f22f04d3cc953729f7bb6e13c517e3888e94
Dariashaa <142736987+Dariashaa@users.noreply.github.com>
2025-09-24 01:14:57 +0300

    Update Primes.java

--- Primes.java
+++ Primes.java
@@ -9,11 +9,12 @@ public class Primes{
         
     public static boolean isPrime(int n){
         if (n < 2) return false;
-        for (int i=2; i<Math.sqrt(n); i++){
+        for (int i=2; i<=Math.sqrt(n); i++){
             if (n%i==0){
                 return false;
             }
         }
         return true;
     }
+
 }
